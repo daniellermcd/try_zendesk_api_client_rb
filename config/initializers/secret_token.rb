b698a9fb8b023549d5aa785d8e41b5c76f244e9e
Danielle Miller <dmiller@optoro.com>
2017-10-26 15:46:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryZendeskApiClientRb::Application.config.secret_token = 'c988a76e72d3565e145d041a0e9c0662c3449f89fd09f6e7846e05c1257a04024c6b9e461bc786a47fede7b1e829704456a5de62f793d065d834a07dc7504732'
